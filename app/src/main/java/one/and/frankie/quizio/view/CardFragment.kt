a4a03284d91a44266f91f2e73242e0bd471e6442
Frankie <frankie.and.one@protonmail.com>
2019-06-01 21:18:50 +0100

    Added card.
    Added card flip animations.

---
+++ app/src/main/java/one/and/frankie/quizio/view/CardFragment.kt
@@ -0,0 +1,51 @@
+package one.and.frankie.quizio.view
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import one.and.frankie.quizio.R
+
+class CardFragment : Fragment() {
+    private var isFrontVisible = true
+    private lateinit var flipInAnim: AnimatorSet
+    private lateinit var flipOutAnim: AnimatorSet
+    private lateinit var cardFront: View
+    private lateinit var cardBack: View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_card, container, false)
+        cardFront = rootView.findViewById(R.id.card_front)
+        cardBack = rootView.findViewById(R.id.card_back)
+        flipInAnim = AnimatorInflater.loadAnimator(rootView.context, R.animator.flip_in) as AnimatorSet
+        flipOutAnim = AnimatorInflater.loadAnimator(rootView.context, R.animator.flip_out) as AnimatorSet
+        val distance = 8000
+        val scale = resources.displayMetrics.density * distance
+        cardFront.cameraDistance = scale
+        cardBack.cameraDistance = scale
+        rootView.setOnClickListener {
+            flipCard()
+        }
+        return rootView
+    }
+
+    private fun flipCard() = when {
+        isFrontVisible -> {
+            flipOutAnim.setTarget(cardFront)
+            flipInAnim.setTarget(cardBack)
+            flipOutAnim.start()
+            flipInAnim.start()
+            isFrontVisible = false
+        }
+        else -> {
+            flipOutAnim.setTarget(cardBack)
+            flipInAnim.setTarget(cardFront)
+            flipOutAnim.start()
+            flipInAnim.start()
+            isFrontVisible = true
+        }
+    }
+}
---
+++ app/src/main/java/one/and/frankie/quizio/view/MainActivity.kt
@@ -0,0 +1,21 @@
+package one.and.frankie.quizio.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import one.and.frankie.quizio.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cardFragment: CardFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initQuestionViewFragment()
+    }
+
+    private fun initQuestionViewFragment() {
+        cardFragment = CardFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fl_container, cardFragment).commit()
+    }
+}
