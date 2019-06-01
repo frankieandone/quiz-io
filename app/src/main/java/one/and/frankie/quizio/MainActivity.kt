f40f0bbe5af13b65f32b0d1e3ffb68b325bd7f40
Frankie <frankie.and.one@protonmail.com>
2019-06-01 19:59:59 +0100

    Only build.gradle (app) is significant.

---
+++ app/src/main/java/one/and/frankie/quizio/MainActivity.kt
@@ -0,0 +1,12 @@
+package one.and.frankie.quizio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
